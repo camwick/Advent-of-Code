17adba1ac7a4d53935ca4ceb061e668aa88aa511
camwick <cwickers@pnw.edu>
2021-12-05 13:53:54 -0600

    puzzle finish

--- Day 4/GiantSquid02.java
+++ Day 4/GiantSquid02.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class GiantSquid01 {
+public class GiantSquid02 {
   public static class Number {
     public int num;
     public boolean hit;
@@ -44,7 +44,7 @@ public class GiantSquid01 {
 
       for (int i = 0; i < board.length; ++i) {
         for (int j = 0; j < board[i].length; ++j) {
-          board[i][j] = new GiantSquid01.Number(boardNumbers[(i * 5) + j]);
+          board[i][j] = new GiantSquid02.Number(boardNumbers[(i * 5) + j]);
         }
       }
     }
@@ -74,7 +74,7 @@ public class GiantSquid01 {
         int counter = 0;
 
         for (int j = 0; j < this.board.length; ++j) {
-          if (this.board[i][j].isHit())
+          if (this.board[j][i].isHit())
             counter++;
         }
 
@@ -83,27 +83,6 @@ public class GiantSquid01 {
           return true;
         }
       }
-
-      // // check diagonals
-      // int counter = 0;
-      // for (int i = 0; i < 5; ++i) {
-      // if (this.board[i][i].isHit())
-      // counter++;
-      // }
-      // if (counter == 5) {
-      // this.win = true;
-      // return true;
-      // }
-
-      // counter = 0;
-      // for (int i = 4; i >= 0; --i) {
-      // if (this.board[i][i].isHit())
-      // counter++;
-      // }
-      // if (counter == 5) {
-      // this.win = true;
-      // return true;
-      // }
 
       return false;
     }
@@ -166,10 +145,7 @@ public class GiantSquid01 {
     }
 
     // grab boards
-    int counter = 0;
-    int boardAmount = 0;
     ArrayList<Board> boards = new ArrayList<>();
-    Board temp;
     while (input.hasNextLine()) {
       int[] tempBoard = new int[25];
 
@@ -178,7 +154,7 @@ public class GiantSquid01 {
       }
       input.nextLine();
 
-      boards.add(new GiantSquid01.Board(tempBoard));
+      boards.add(new GiantSquid02.Board(tempBoard));
     }
 
     for (Board x : boards) {
@@ -186,15 +162,26 @@ public class GiantSquid01 {
     }
 
     boolean loop = false;
+    int count = 0;
     int winningNum = 0;
+    int winningIndex = 0;
+
     for (int j = 0; j < drawnNums.length; ++j) {
       for (int i = 0; i < boards.size(); ++i) {
+        if (boards.get(i).getWinningBoard())
+          continue;
+
         boards.get(i).hit(drawnNums[j]);
-        counter++;
+
         if (boards.get(i).checkBingo()) {
-          loop = true;
+          count++;
           winningNum = drawnNums[j];
-          break;
+
+          if (count == boards.size()) {
+            loop = true;
+            winningIndex = i;
+            break;
+          }
         }
       }
 
@@ -202,13 +189,7 @@ public class GiantSquid01 {
         break;
     }
 
-    Board winningBoard = new GiantSquid01.Board();
-    for (Board x : boards) {
-      if (x.getWinningBoard()) {
-        winningBoard = x;
-        break;
-      }
-    }
+    Board winningBoard = boards.get(winningIndex);
 
     System.out.println("Winning Board:\n" + winningBoard);
     System.out.println("Won on number: " + winningNum);
