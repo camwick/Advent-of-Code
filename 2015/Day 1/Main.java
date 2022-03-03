397d336e8167708cc5e6339839066b28f414fd09
camwick <cwickers@pnw.edu>
2022-03-03 13:11:42 -0600

    2015.1.2

--- 2015/Day 1/Main.java
+++ 2015/Day 1/Main.java
@@ -14,20 +14,27 @@ public class Main {
             scan.close();
 
             // solution
-            int ans = 0;
+            int ans1 = 0;
+            int ans2 = 0;
+            boolean notFound = true;
 
             for (int i = 0; i < input.length(); ++i) {
                 switch (input.charAt(i)) {
                     case '(':
-                        ans++;
+                        ans1++;
                         break;
                     case ')':
-                        ans--;
+                        ans1--;
                         break;
+                }
+
+                if (notFound && ans1 == -1) {
+                    ans2 = i + 1;
+                    notFound = false;
                 }
             }
 
-            System.out.println(ans);
+            System.out.println(ans1 + "\n" + ans2);
 
         } catch (FileNotFoundException e) {
             System.out.println(e);
