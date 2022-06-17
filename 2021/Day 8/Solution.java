7fa0724ba14f782f7ed5fb0c5398152cbc21c170
camwick <cwickers@pnw.edu>
2022-06-17 15:34:18 -0500

    part 1

--- 2021/Day 8/Solution.java
+++ 2021/Day 8/Solution.java
@@ -1,3 +1,57 @@
-public static void main(String[] args) {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file;
+        Scanner input;
+
+        if (args.length > 0)
+            file = new File(args[0]);
+        else
+            file = new File("input.txt");
+
+        input = new Scanner(file);
+
+        // parse input
+        List<String> lines = new ArrayList<>();
+        while (input.hasNextLine()) {
+            lines.add(input.nextLine());
+        }
+
+        input.close();
+
+        // PART 1
+        long start = System.currentTimeMillis();
+
+        int count = 0;
+        for (int i = 0; i < lines.size(); ++i) {
+            String[] parts = lines.get(i).split(" \\| ");
+            String[] segments = parts[1].split(" ");
+
+            for (String segment : segments) {
+                switch (segment.length()) {
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 7:
+                        count++;
+                }
+            }
+        }
+
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println("Count: " + count);
+        System.out.println("Elapsed time: " + elapsed + "ms");
+
+        // PART 2
+        start = System.currentTimeMillis();
+
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println("Sum: ");
+        System.out.println("Elapsed time: " + elapsed + "ms");
+    }
 }
