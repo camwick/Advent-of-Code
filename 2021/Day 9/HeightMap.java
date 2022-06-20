eebc3e951382b657a742349179e8909d877837d4
camwick <cwickers@pnw.edu>
2022-06-19 21:38:42 -0500

    name change + finishing touches

---
+++ 2021/Day 9/HeightMap.java
@@ -0,0 +1,39 @@
+public class HeightMap {
+    private int contents;
+    private HeightMap[] neighbors;
+    private boolean corner;
+    private boolean visited;
+
+    public HeightMap(int x) {
+        this.contents = x;
+        this.visited = x == 9;
+        this.corner = false;
+        this.neighbors = new HeightMap[4];
+    }
+
+    public int getContents() {
+        return this.contents;
+    }
+
+    public boolean getVisited() {
+        return this.visited;
+    }
+
+    public boolean getCorner() {
+        return this.corner;
+    }
+
+    public HeightMap[] getNeighbors() {
+        return this.neighbors;
+    }
+
+    public void setNeighbors(HeightMap[] x) {
+        for (int i = 0; i < x.length; ++i) {
+            this.neighbors[i] = x[i];
+        }
+    }
+
+    public void setCorner(boolean x) {
+        this.corner = x;
+    }
+}
